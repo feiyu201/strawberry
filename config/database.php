6a2433526cfe78982cc24a625d2fc38a68a6b53e
jackfan <info@aim-china.com>
2020-09-25 19:58:28 +0800

    rename config/database.php.example to config/database.php.

--- config/database.php
+++ config/database.php
@@ -23,11 +23,11 @@ return [
             // 服务器地址
             'hostname'          => env('database.hostname', '127.0.0.1'),
             // 数据库名
-            'database'          => env('database.database', 'stadmin'),
+            'database'          => env('database.database', 'strawberrydb'),
             // 用户名
             'username'          => env('database.username', 'root'),
             // 密码
-            'password'          => env('database.password', 'root'),
+            'password'          => env('database.password', ''),
             // 端口
             'hostport'          => env('database.hostport', '3306'),
             // 数据库连接参数
@@ -35,7 +35,7 @@ return [
             // 数据库编码默认采用utf8
             'charset'           => env('database.charset', 'utf8'),
             // 数据库表前缀
-            'prefix'            => env('database.prefix', 'super_'),
+            'prefix'            => env('database.prefix', 'st_'),
 
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
             'deploy'            => 0,
