b3f477d637409dfa4cf453adcf1c8597659e7ef9
jackfan <info@aim-china.com>
2020-11-12 19:31:01 +0800

    增加插件安装状态表

--- public/cmdatabase.sql
+++ public/cmdatabase.sql
@@ -256,6 +256,27 @@ CREATE TABLE `cm_wxapp` (
   `updatetime` int(10) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `cm_plugin`
+--
+
+CREATE TABLE `cm_plugin` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL DEFAULT '' COMMENT '插件标志',
+  `title` varchar(32) NOT NULL DEFAULT '' COMMENT '插件名称',
+  `description` text NOT NULL COMMENT '插件描述',
+  `author` varchar(32) NOT NULL DEFAULT '' COMMENT '作者',
+  `version` varchar(16) NOT NULL COMMENT '版本号',
+  `createtime` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '安装时间',
+  `updatetime` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+  `sort` int(11) NOT NULL DEFAULT '100' COMMENT '排序',
+  `status` tinyint(2) NOT NULL DEFAULT '1' COMMENT '状态',
+  `install` tinyint(2) NOT NULL DEFAULT '1' COMMENT '是否安装',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='插件表';
 
 --
 -- 转储表的索引
