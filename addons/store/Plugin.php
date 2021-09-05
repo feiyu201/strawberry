4f851c42d1b97b2f0bf42738e12eb33302da8042
jackfan <info@aim-china.com>
2021-09-05 11:45:11 +0000

    优化商品插件

--- addons/store/Plugin.php
+++ addons/store/Plugin.php
@@ -37,7 +37,6 @@ class Plugin extends Addons	// 需继承think\Addons类
                     ['name' => 'addons/store/category/category', 'title' => '商品分类','ismenu'=> 1],
                     ['name' => 'addons/store/brand/list', 'title' => '品牌列表','ismenu'=> 1],
                     ['name' => 'addons/store/goods/list', 'title' => '商品列表','ismenu'=> 1],
-                    ['name' => 'addons/store/category/getCategory', 'title' => '分类接口例子','ismenu'=> 1],
                 ]
             ]
         ];
