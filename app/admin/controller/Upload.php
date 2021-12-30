3bacbab1c2f16db3feb65a3b3a16260fdeb5c77c
lihongxiu <2137584816@qq.com>
2021-12-30 22:59:52 +0800

    upload

--- app/admin/controller/Upload.php
+++ app/admin/controller/Upload.php
@@ -110,22 +110,28 @@ class Upload extends BaseController
     public function attachment()
     {
         //钩子事件 存储插件
-        Event::listen('Storage', 'addons\qiniu_storage\event\Storage');
-        $hook_res = event('Storage')[0];
-        if ($hook_res) {
-            $url       = $hook_res['url'];
-            $storage   = $hook_res['storage'];
-            $savename  = $hook_res['fileName'];
-            $file_path = $hook_res['fileGetRealPath'];
-            $mimetype  = $hook_res['fileGetOriginalMime'];
-//            dd($hook_res); //本地存储逻辑也可以写钩子里面
-        } else {
-            $file      = request()->file('file');
-            $savename  = \think\facade\Filesystem::disk('public')->putFile('attachment', $file);
-            $url = $file_path = 'storage/' . $savename;
-            $storage = 'localhost';
-            $mimetype  = $file->getOriginalExtension();  //mime_content_type 5.3已经废弃
-        }
+        //Event::listen('Storage', 'addons\qiniu_storage\event\Storage');
+//        $hook_res = event('Storage')[0];
+//        if ($hook_res) {
+//            $url       = $hook_res['url'];
+//            $storage   = $hook_res['storage'];
+//            $savename  = $hook_res['fileName'];
+//            $file_path = $hook_res['fileGetRealPath'];
+//            $mimetype  = $hook_res['fileGetOriginalMime'];
+////            dd($hook_res); //本地存储逻辑也可以写钩子里面
+//        } else {
+//            $file      = request()->file('file');
+//            $savename  = \think\facade\Filesystem::disk('public')->putFile('attachment', $file);
+//            $url = $file_path = 'storage/' . $savename;
+//            $storage = 'localhost';
+//            $mimetype  = $file->getOriginalExtension();  //mime_content_type 5.3已经废弃
+//        }
+
+        $file      = request()->file('file');
+        $savename  = \think\facade\Filesystem::disk('public')->putFile('attachment', $file);
+        $url = $file_path = 'storage/' . $savename;
+        $storage = 'localhost';
+        $mimetype  = $file->getOriginalExtension();  //mime_content_type 5.3已经废弃
         $add['url'] = '/'.$url;
         $add['storage'] = $storage;
         $add['filesize'] = filesize($file_path);
@@ -145,9 +151,12 @@ class Upload extends BaseController
 
         $key = md5(time().rand(1000, 9999));
         Cache::set($key, $add);
-        if ($this->request->param('save')==1) {
-            Db::name('attachment')->insert($add);
-        }
+//        echo "<pre>";
+//        var_dump($this->request->param('save'));
+//        echo "<br/>";
+        //if ($this->request->param('save')==1) {
+        Db::name('attachment')->insert($add);
+        //}
       
         // 将上传后的文件位置返回给前端
         return json(['code' => 0, 'path' => $savename,'key'=>$key]);
